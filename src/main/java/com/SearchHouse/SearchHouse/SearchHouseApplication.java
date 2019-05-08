5bb4fe65a1b2a20271a1693446e2d21db3271dcb
Philippe-Jefferson <pguesssle@gmail.com>
2019-05-08 15:55:06 +0200

    initialized spring boot app and maven

---
+++ src/main/java/com/SearchHouse/SearchHouse/SearchHouseApplication.java
@@ -0,0 +1,13 @@
+package com.SearchHouse.SearchHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchHouseApplication.class, args);
+	}
+
+}
